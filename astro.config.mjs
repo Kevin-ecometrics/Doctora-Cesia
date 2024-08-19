e954fe6d0ba1518c239e685878e0fbbf5f9e7274
Superuser <kevin@e-commetrics.com>
2024-08-19 14:01:11 -0700

    update version sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: squooshImageService()
   },
-  integrations: [react(), tailwind(), partytown()]
+  site: "https://cesiaborjon.com/",
+  integrations: [react(), tailwind(), partytown(), sitemap(), robotsTxt()]
 });
